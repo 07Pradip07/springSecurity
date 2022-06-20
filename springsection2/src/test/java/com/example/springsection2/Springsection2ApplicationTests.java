50825a49113036cf30fe00a0a032cbc3979ce222
pradip <pradipp39@gmail.com>
2022-06-20 12:20:22 +0530

    section2 initializ

---
+++ springsection2/src/test/java/com/example/springsection2/Springsection2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsection2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
